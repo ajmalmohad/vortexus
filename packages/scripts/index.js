b23feea31fbbea16c969872f38ed05ae6da1d4d1
ajmalmohad <ajmalajmal.2016@gmail.com>
2024-01-18 19:37:37 +0530

    feat: serve static files

--- packages/scripts/index.js
+++ packages/scripts/index.js
@@ -1,37 +1,25 @@
 #! /usr/bin/env node
-
-import { createServer } from 'http';
-import * as path from 'path';
-import { createServer as createViteServer } from 'vite';
-
-let vite;
-
-const server = createServer(async (request, response) => {
-    if (vite) {
-        return vite.middlewares(request, response);
-    }
+import handler from 'serve-handler';
+import http from 'http';
+const server = http.createServer((request, response) => {
+  return handler(request, response, {
+    "public": "build"
+  });
 });
-
-export const startServer = (port) => {
-    server.listen(port);
-    console.log('Server running at http://127.0.0.1:' + port + '/');
-}
 
 const [task] = process.argv.slice(2);
 
 switch (task) {
   case "dev": {
-    createViteServer({ 
-        server: { middlewareMode: 'html' },
-        root: path.resolve('build')
-    }).then(server => {
-        vite = server;
-        startServer(3000);
+    server.listen(3000, () => {
+      console.log('Running at http://localhost:3000');
     });
     break;
   }
-  case "build": {
-    console.log("build");
+  case "start": {
+    server.listen(5000, () => {
+      console.log('Running at http://localhost:5000');
+    });
     break;
   }
   default:
